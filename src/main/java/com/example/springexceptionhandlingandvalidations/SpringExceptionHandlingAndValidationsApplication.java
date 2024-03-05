a1be90850d4c4cd743cb026bb496cd5e5545412c
SandeepSathyala <sandybalu001@gmail.com>
2024-03-05 10:04:48 +0530

    updated

---
+++ src/main/java/com/example/springexceptionhandlingandvalidations/SpringExceptionHandlingAndValidationsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springexceptionhandlingandvalidations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExceptionHandlingAndValidationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExceptionHandlingAndValidationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springexceptionhandlingandvalidations/SpringExceptionHandlingAndValidationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springexceptionhandlingandvalidations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExceptionHandlingAndValidationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
