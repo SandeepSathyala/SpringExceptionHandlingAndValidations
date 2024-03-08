046cbc9bb6135e38301b909bdd174a85aa413b22
SandeepSathyala <sandybalu001@gmail.com>
2024-03-08 17:04:11 +0530

    validations updated

--- src/main/java/com/example/springexceptionhandlingandvalidations/exception/CustomizedMsException.java
+++ src/main/java/com/example/springexceptionhandlingandvalidations/exception/CustomizedMsException.java
@@ -9,11 +9,13 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 @RestControllerAdvice
 public class CustomizedMsException extends ResponseEntityExceptionHandler {
+
     @ExceptionHandler(Exception.class)
     public final ResponseEntity<ErrorDetails> handleAllExceptions(Exception ex, WebRequest request){
         ErrorDetails errorsDetails = new ErrorDetails(ex.getMessage(), 500L,null);
         return new ResponseEntity<>(errorsDetails, HttpStatus.INTERNAL_SERVER_ERROR);
     }
+
     @ExceptionHandler(ProcessingException.class)
     public  final ResponseEntity<ErrorDetails> handleProcessingException(ProcessingException ex, WebRequest request){
         ErrorDetails errorsDetails = new ErrorDetails(ex.getMessage(), ex.getCode(),null);
